d6a826ecbc27b76e5a745a76f6d5ade22dd1e650
pbaffiliatetwoop <pbaffiliate2@gmail.com>
2016-02-18 08:31:32 +0200

    Commit from Test -  src/scripts/testGetListLatestScript.js

--- src/scripts/testGetListLatestScript.js
+++ src/scripts/testGetListLatestScript.js
@@ -1,14 +1,1 @@
-<script>
-<scriptACL>
-<execute>all</execute>
-<read>all</read>
-<write>all</write>
-</scriptACL>
-<code> 
-<![CDATA[	
-         
-return "ace of base";
-
-]]>
-</code>
-</script>
+testGetListLatestScript
